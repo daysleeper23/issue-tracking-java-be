f8bab89a44cc1ce6ad1f61dcfe74864e324a8eec
madaraDance <maksim.minenko97@gmail.com>
2024-11-13 12:25:08 +0200

    Added date validation to projectDTO

--- src/main/java/org/projectmanagement/application/dto/Project/ProjectMapper.java
+++ src/main/java/org/projectmanagement/application/dto/Project/ProjectMapper.java
@@ -1,12 +1,17 @@
 package org.projectmanagement.application.dto.Project;
 
+import net.bytebuddy.implementation.bytecode.Throw;
 import org.projectmanagement.domain.entities.Projects;
 
+import java.time.Instant;
 import java.util.UUID;
 
 public class ProjectMapper {
 
     public static Projects createDTOToProject(ProjectCreateDTO dto) {
+        if (dto.getStartDate() != null && dto.getEndDate() != null) {
+            validateDates(dto.getStartDate(), dto.getEndDate());
+        }
         return Projects.builder()
                 .name(dto.getName())
                 .description(dto.getDescription())
@@ -19,6 +24,9 @@ public class ProjectMapper {
     }
 
     public static Projects updateDTOToProject(UUID id, ProjectUpdateDTO dto) {
+        if (dto.getStartDate() != null && dto.getEndDate() != null) {
+            validateDates(dto.getStartDate(), dto.getEndDate());
+        }
         return Projects.builder()
                 .id(id)
                 .description(dto.getDescription())
@@ -27,5 +35,11 @@ public class ProjectMapper {
                 .priority(dto.getPriority())
                 .status(dto.getStatus())
                 .build();
+    }
+
+    public static void validateDates(Instant startDate, Instant endDate) {
+        if(startDate.isAfter(endDate)) {
+            throw new IllegalArgumentException("Start date must not be after end date.");
+        }
     }
 }
