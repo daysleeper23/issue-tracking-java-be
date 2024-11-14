dbc3217e9c20b78beb2dd3b158cb6c3cfe8b4c71
lvqthuong <lvq.thuong@gmail.com>
2024-11-14 16:32:19 +0200

    [Feature][PM-02] Company CRUD

---
+++ src/main/java/org/projectmanagement/application/dto/companies/CompanyDTO.java
@@ -0,0 +1,8 @@
+package org.projectmanagement.application.dto.companies;
+
+import jakarta.validation.constraints.NotBlank;
+
+public record CompanyDTO(
+        @NotBlank(message = "Company name must not be empty")String name,
+        String description) {
+}
---
+++ src/main/java/org/projectmanagement/presentation/controllers/CompaniesController.java
@@ -0,0 +1,58 @@
+package org.projectmanagement.presentation.controllers;
+
+import jakarta.validation.Valid;
+import org.projectmanagement.application.dto.companies.CompanyDTO;
+import org.projectmanagement.domain.entities.Companies;
+import org.projectmanagement.domain.services.CompaniesService;
+import org.projectmanagement.presentation.response.GlobalResponse;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.stereotype.Repository;
+import org.springframework.web.bind.annotation.*;
+
+@RequestMapping("/companies")
+@RestController
+public class CompaniesController {
+    private final CompaniesService companiesService;
+
+    public CompaniesController(CompaniesService companiesService){
+        this.companiesService = companiesService;
+    }
+
+    @PostMapping
+    public ResponseEntity<GlobalResponse<Companies>> createCompany(
+            @RequestBody @Valid CompanyDTO dto
+            ){
+        return new ResponseEntity<>(
+                new GlobalResponse<>(HttpStatus.CREATED.value(), companiesService.createNewCompany(dto)),
+                HttpStatus.CREATED);
+    }
+
+    @GetMapping("{id}")
+    public ResponseEntity<GlobalResponse<Companies>> getCompany(
+            @PathVariable String id
+    ){
+        return new ResponseEntity<>(
+                new GlobalResponse<>(HttpStatus.OK.value(), companiesService.getCompany(id)),
+                HttpStatus.OK
+        );
+    }
+
+    @PutMapping("{id}")
+    public ResponseEntity<GlobalResponse<Companies>> updateCompany(
+            @PathVariable String id,
+            @RequestBody @Valid CompanyDTO dto
+    ){
+        return new ResponseEntity<>(
+                new GlobalResponse<>(HttpStatus.OK.value(), companiesService.updateCompany(id,dto)),
+                HttpStatus.OK
+        );
+    }
+
+//    @DeleteMapping("{id}")
+//    public ResponseEntity<GlobalResponse<Boolean>> archiveCompany(
+//            @PathVariable @Valid String id
+//    ){
+//        return ResponseEntity.ok(new GlobalResponse<>(HttpStatus.OK.value(), companiesService.archiveCompany(id)));
+//    }
+}
