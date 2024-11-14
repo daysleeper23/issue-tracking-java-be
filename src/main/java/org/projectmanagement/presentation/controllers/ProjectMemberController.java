4d17eae670483717962e250b660bbd4a812ed9e2
madaraDance <maksim.minenko97@gmail.com>
2024-11-14 16:25:39 +0200

    Add ProjectMemberController to handle ProjectMember operations

--- src/main/java/org/projectmanagement/presentation/controllers/ProjectMemberController.java
+++ src/main/java/org/projectmanagement/presentation/controllers/ProjectMemberController.java
@@ -1,16 +1,16 @@
 package org.projectmanagement.presentation.controllers;
 
 import jakarta.validation.Valid;
-import org.projectmanagement.application.dto.Project.ProjectCreateDTO;
-import org.projectmanagement.application.dto.ProjectMember.ProjectMemberCreateDTO;
+import org.projectmanagement.application.dto.project_members.ProjectMemberCreateDTO;
 import org.projectmanagement.application.services.ProjectMembersServiceImpl;
 import org.projectmanagement.domain.entities.ProjectMembers;
-import org.projectmanagement.domain.entities.Projects;
+import org.projectmanagement.presentation.response.GlobalResponse;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
+import java.util.Optional;
 import java.util.UUID;
 
 @RestController
@@ -22,13 +22,45 @@ public class ProjectMemberController {
         this.projectMembersService = projectMembersService;
     }
 
-    @GetMapping("/{id}")
-    public ResponseEntity<GlobalResponse<List<ProjectMembers>>> getMembersByProjectId(@PathVariable UUID id) {
-        return new ResponseEntity<>(new GlobalResponse<>(HttpStatus.OK.value(), projectMembersService.getMembersByProjectId(id)), HttpStatus.OK);
+    //naming for mapping is hard -_-
+    //return all members of given project
+    @GetMapping("byProject/{projectId}")
+    public ResponseEntity<GlobalResponse<List<ProjectMembers>>> getAllMembersByProjectId(@Valid @PathVariable UUID projectId) {
+        return new ResponseEntity<>(new GlobalResponse<>(HttpStatus.OK.value(), projectMembersService.getAllProjectMembersByProjectId(projectId)), HttpStatus.OK);
     }
 
-    @PostMapping
-    public ResponseEntity<GlobalResponse<ProjectMembers>> createProjectMember(@RequestBody @Valid ProjectMemberCreateDTO projectMember) {
-        return new ResponseEntity<>(new GlobalResponse<>(HttpStatus.CREATED.value(), projectMembersService.createProjectMember(projectMember)), HttpStatus.CREATED);
+    //return all projects of a given member
+    @GetMapping("byUser/{userId}")
+    public ResponseEntity<GlobalResponse<List<ProjectMembers>>> getAllProjectsMemberIsPartOfByUserId(@Valid @PathVariable UUID userId) {
+        return new ResponseEntity<>(new GlobalResponse<>(HttpStatus.OK.value(), projectMembersService.getAllProjectsMemberIsPartOfByUserId(userId)), HttpStatus.OK);
     }
+
+/*
+    Not implemented until we decide upon mapstruct
+    @PostMapping
+    public ResponseEntity<GlobalResponse<ProjectMembers>> createProjectMember(@Valid @PathVariable  userId, @Valid ProjectMemberCreateDTO dto) {
+        return new ResponseEntity<>(new GlobalResponse<>(HttpStatus.OK.value(), projectMembersService.createProjectMember(dto)), HttpStatus.OK);
+    }
+
+    @PatchMapping("{id}")
+    public ResponseEntity<GlobalResponse<ProjectMembers>> updateProjectMember(@Valid @PathVariable  userId, @Valid ProjectMemberUpdateDTO dto) {
+        return new ResponseEntity<>(new GlobalResponse<>(HttpStatus.OK.value(), projectMembersService.updateProjectMember(dto)), HttpStatus.OK);
+    }
+
+*/
+    @DeleteMapping("{id}")
+    public ResponseEntity<GlobalResponse<Void>> deleteProjectMemberById(@Valid @PathVariable UUID id) {
+        projectMembersService.deleteProjectMemberById(id);
+        return new ResponseEntity<>(new GlobalResponse<>(HttpStatus.OK.value(), null), HttpStatus.OK);
+    }
+
+    @DeleteMapping("/{projectId}/{userId}")
+    public ResponseEntity<GlobalResponse<Void>> deleteProjectMemberByProjectIdAndUserId(@Valid @PathVariable UUID projectId,@Valid @PathVariable UUID userId) {
+        projectMembersService. deleteProjectMemberByProjectIdAndUserId(projectId, userId);
+        return new ResponseEntity<>(new GlobalResponse<>(HttpStatus.OK.value(), null), HttpStatus.OK);
+    }
+
+
+
+
 }
