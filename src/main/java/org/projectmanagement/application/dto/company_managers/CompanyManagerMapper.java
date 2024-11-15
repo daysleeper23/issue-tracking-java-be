c37d24c94a2e1c276c599ca8ce38117d92177922
madaraDance <maksim.minenko97@gmail.com>
2024-11-15 15:42:23 +0200

    created dtos and mapper for CompanyManagers

---
+++ src/main/java/org/projectmanagement/application/dto/company_managers/CompanyManagerMapper.java
@@ -0,0 +1,17 @@
+package org.projectmanagement.application.dto.company_managers;
+
+import org.mapstruct.*;
+import org.mapstruct.factory.Mappers;
+import org.projectmanagement.domain.entities.CompanyManagers;
+
+@Mapper(nullValuePropertyMappingStrategy = NullValuePropertyMappingStrategy.IGNORE)
+public interface CompanyManagerMapper {
+
+    CompanyManagerMapper INSTANCE = Mappers.getMapper(CompanyManagerMapper.class);
+
+    @Mapping(target = "id", ignore = true)
+    @Mapping(target = "createdAt", ignore = true)
+    @Mapping(target = "updatedAt", ignore = true)
+    void updateCompanManagersDTOToCompanyManagers(UpdateCompanyManagersDTO dto, @MappingTarget CompanyManagers companyManager);
+
+}
