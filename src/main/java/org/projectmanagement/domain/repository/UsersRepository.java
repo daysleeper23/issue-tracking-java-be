290f07035c250f0ee76750182784682940fd7e5c
daysleeper23 <charnel1986@gmail.com>
2024-11-19 12:18:36 +0200

    Implement Jpa for Users

--- src/main/java/org/projectmanagement/domain/repository/UsersRepository.java
+++ src/main/java/org/projectmanagement/domain/repository/UsersRepository.java
@@ -5,20 +5,14 @@ import java.util.Optional;
 import java.util.UUID;
 
 import org.projectmanagement.domain.entities.Users;
-import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.data.jpa.repository.Query;
 
-public interface UsersRepository extends JpaRepository<Users, UUID> {
+public interface UsersRepository {
 
-//  Users save(Users user);
+  Users save(Users user);
 
-//  Users safeCopy(Users user);
-
-  @Query("SELECT u FROM Users u WHERE u.companyId = :companyId")
   List<Users> findAllFromCompany(UUID companyId);
 
-//  @Query("SELECT u FROM Users u WHERE u.id = :id")
-//  Optional<Users> findById(UUID id);
+  Optional<Users> findById(UUID id);
 
-//  void deleteById(UUID id);
+  void deleteById(UUID id);
 }
