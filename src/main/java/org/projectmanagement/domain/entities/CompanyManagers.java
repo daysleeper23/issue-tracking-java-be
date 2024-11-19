1f89bd973c72acaf486410efc24aa22a415cd6ea
madaraDance <maksim.minenko97@gmail.com>
2024-11-19 13:01:41 +0200

    converted RolesPermissions and CompanyManagers to entity

--- src/main/java/org/projectmanagement/domain/entities/CompanyManagers.java
+++ src/main/java/org/projectmanagement/domain/entities/CompanyManagers.java
@@ -1,30 +1,50 @@
 package org.projectmanagement.domain.entities;
 
+import jakarta.persistence.*;
 import lombok.*;
 
 import java.time.Instant;
 import java.util.UUID;
 
-@ToString
+@Entity
+@Table(name = "company_managers")
 @Getter
 @Setter
+@NoArgsConstructor
+@AllArgsConstructor
 @Builder
+@ToString
 public class CompanyManagers {
+    @Id
+    @GeneratedValue(generator = "UUID")
     private UUID id;
 
     @NonNull
+    @Column(name = "user_id", nullable = false)
     private UUID userId;
 
     @NonNull
+    @Column(name = "company_id", nullable = false)
     private UUID companyId;
 
     @NonNull
+    @Column(name = "role_id", nullable = false)
     private UUID roleId;
 
-    @NonNull
-    private final Instant createdAt;
 
-    @NonNull
+    private  Instant createdAt;
+
+
     private Instant updatedAt;
+
+    @PrePersist
+    protected void onCreate() {
+        this.createdAt = Instant.now();
+    }
+
+    @PreUpdate
+    protected void onUpdate() {
+        this.updatedAt = Instant.now();
+    }
 
 }
