e31d7fc1b618c501183909b30fc32bfe557ffa03
madaraDance <maksim.minenko97@gmail.com>
2024-11-19 15:18:35 +0200

    create jpa interface for roles permissions and modified the same for proejctMembers and projects

--- src/main/java/org/projectmanagement/domain/repository/ProjectMembersJpaRepo.java
+++ src/main/java/org/projectmanagement/domain/repository/ProjectMembersJpaRepo.java
@@ -2,6 +2,8 @@ package org.projectmanagement.domain.repository;
 
 import org.projectmanagement.domain.entities.ProjectMembers;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
 
 import java.util.List;
@@ -9,7 +11,23 @@ import java.util.UUID;
 
 @Repository
 public interface ProjectMembersJpaRepo extends JpaRepository<ProjectMembers, UUID> {
-    List<ProjectMembers> findAllByProjectId(UUID projectId);
-    List<ProjectMembers> findAllByUserId(UUID userId);
-    void deleteByProjectIdAndUserId(UUID projectId, UUID userId);
+    @Query(value = "SELECT * " +
+            "FROM project_members pm " +
+            "WHERE pm.project_id = :projectId ",
+            nativeQuery = true)
+    List<ProjectMembers> findAllByProjectId(@Param("projectId") UUID projectId);
+
+    @Query(value = "SELECT * " +
+            "FROM project_members pm " +
+            "WHERE pm.user_id = :userId ",
+            nativeQuery = true)
+    List<ProjectMembers> findAllByUserId(@Param("userId") UUID userId);
+
+
+    @Query(value = "DELETE " +
+            "FROM project_members pm " +
+            "WHERE pm.project_id = :projectId " +
+            "AND pm.user_id = :userId",
+            nativeQuery = true)
+    void deleteByProjectIdAndUserId(@Param("projectId")UUID projectId, @Param("userId") UUID userId);
 }
