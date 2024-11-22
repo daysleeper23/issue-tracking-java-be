4d525dd75595ab44f72ea09d1dd135651397c11c
madaraDance <maksim.minenko97@gmail.com>
2024-11-22 11:57:52 +0200

    implemented fixes found from testing

--- src/main/java/org/projectmanagement/application/services/CompanyManagersServiceImpl.java
+++ src/main/java/org/projectmanagement/application/services/CompanyManagersServiceImpl.java
@@ -6,11 +6,13 @@ import org.projectmanagement.application.dto.company_managers.UpdateCompanyManag
 import org.projectmanagement.domain.entities.Companies;
 import org.projectmanagement.domain.entities.CompanyManagers;
 import org.projectmanagement.domain.entities.Roles;
+import org.projectmanagement.domain.entities.Users;
 import org.projectmanagement.domain.exceptions.InvalidInputException;
 import org.projectmanagement.domain.exceptions.ResourceNotFoundException;
 import org.projectmanagement.domain.repository.CompaniesRepository;
 import org.projectmanagement.domain.repository.CompanyManagersRepository;
 import org.projectmanagement.domain.repository.RolesRepository;
+import org.projectmanagement.domain.repository.UsersRepository;
 import org.projectmanagement.domain.services.CompanyManagersService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
@@ -25,20 +27,31 @@ public class CompanyManagersServiceImpl implements CompanyManagersService {
     private final CompanyManagersRepository companyManagersRepository;
     private final RolesRepository rolesRepository;
     private final CompaniesRepository companiesRepository;
+    private final UsersRepository usersRepository;
 
     @Autowired
     CompanyManagersServiceImpl(
         CompanyManagersRepository companyManagersRepository,
         RolesRepository rolesRepository,
-        CompaniesRepository companiesRepository
+        CompaniesRepository companiesRepository,
+        UsersRepository usersRepository
     ) {
         this.companyManagersRepository = companyManagersRepository;
         this.rolesRepository = rolesRepository;
         this.companiesRepository = companiesRepository;
+        this.usersRepository = usersRepository;
     }
 
     @Override
     public CompanyManagers createCompanyManager(CreateCompanyManagers dto) {
+        Roles role = rolesRepository.findById(dto.roleId()).orElseThrow(() -> new ResourceNotFoundException("Role with id: " + dto.roleId() + " is not found"));
+
+        if (!role.getName().equals("ADMIN") && !role.getName().equals("COMPANY_MANAGER")) {
+            throw new InvalidInputException("Company Managers can only be assigned to ADMIN or COMPANY_MANAGER roles.");
+        }
+
+        Users user = usersRepository.findById(dto.userId()).orElseThrow(() -> new ResourceNotFoundException("User with id: " + dto.userId() + " is not found"));
+
         return companyManagersRepository.save(
                 CompanyManagers.builder()
                         .id(UUID.randomUUID())
