8acec3610ee2f310e4d1d109411274e435bf9f46
madaraDance <maksim.minenko97@gmail.com>
2024-11-23 16:43:13 +0200

    Modified jpa and added one more querry

--- src/main/java/org/projectmanagement/domain/repository/PermissionsJpaRepo.java
+++ src/main/java/org/projectmanagement/domain/repository/PermissionsJpaRepo.java
@@ -2,10 +2,17 @@ package org.projectmanagement.domain.repository;
 
 import org.projectmanagement.domain.entities.Permissions;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 
+import java.util.List;
 import java.util.Optional;
 import java.util.UUID;
 
 public interface PermissionsJpaRepo extends JpaRepository<Permissions, UUID> {
     Optional<Permissions> findByName(String name);
+
+    @Query(value = "SELECT id " +
+            "FROM permissions ",
+            nativeQuery = true)
+    List<UUID> findAllPermissionIds();
 }
