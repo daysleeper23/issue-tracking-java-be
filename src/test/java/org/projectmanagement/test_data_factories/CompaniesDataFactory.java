133e4402ce3f146885b9f2847b1fbb7141b8376f
madaraDance <maksim.minenko97@gmail.com>
2024-11-25 16:47:00 +0200

    created data factories

---
+++ src/test/java/org/projectmanagement/test_data_factories/CompaniesDataFactory.java
@@ -0,0 +1,27 @@
+package org.projectmanagement.test_data_factories;
+
+import org.projectmanagement.domain.entities.Companies;
+import org.projectmanagement.domain.repository.jpa.CompaniesJpaRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Component;
+
+import java.util.UUID;
+
+@Component
+public class CompaniesDataFactory {
+
+    @Autowired
+    private CompaniesJpaRepository companiesRepoJpa;
+
+    public void deleteAll() {
+        companiesRepoJpa.deleteAll();
+    }
+
+    public  UUID createCompany() {
+        Companies company = companiesRepoJpa.save(Companies.builder()
+                        .name("Test Company")
+                        .description("Test Description")
+                .build());
+        return company.getId();
+    }
+}
---
+++ src/test/java/org/projectmanagement/test_data_factories/UsersDataFactory.java
@@ -0,0 +1,57 @@
+package org.projectmanagement.test_data_factories;
+
+import org.projectmanagement.domain.entities.Companies;
+import org.projectmanagement.domain.entities.Users;
+import org.projectmanagement.domain.repository.UsersRepoJpa;
+import org.projectmanagement.domain.repository.jpa.CompaniesJpaRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Component;
+
+import java.util.UUID;
+
+@Component
+public class UsersDataFactory {
+    @Autowired
+    private UsersRepoJpa usersRepoJpa;
+    @Autowired
+    private CompaniesJpaRepository companiesJpaRepository;
+
+    public void deleteAll() {
+        usersRepoJpa.deleteAll();
+    }
+
+
+    public UUID createOwnerUser(UUID companyId) {
+        Users user = usersRepoJpa.save(Users.builder()
+                .name("Test User")
+                .email("testuser@example.com")
+                .passwordHash("hashedpassword")
+                .companyId(companyId)
+                .isActive(true)
+                .isOwner(true)
+                .isDeleted(false)
+                .build());
+
+        Companies company = companiesJpaRepository.findById(companyId).orElse(null);
+
+        company.setOwnerId(user.getId());
+
+        companiesJpaRepository.save(company);
+
+        return user.getId();
+    }
+
+    public UUID createNonOwnerUser(UUID companyId) {
+        Users user = usersRepoJpa.save(Users.builder()
+                .name("Test User2")
+                .email("testuser2@example.com")
+                .passwordHash("hashedpassword")
+                .companyId(companyId)
+                .isActive(true)
+                .isOwner(false)
+                .isDeleted(false)
+                .build());
+
+        return user.getId();
+    }
+}
