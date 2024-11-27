bb1862df5dbcd4953ab6c6a6b13a901ed097401a
lvqthuong <lvq.thuong@gmail.com>
2024-11-27 23:30:15 +0200

    integration test java mail sender

---
+++ src/test/java/org/projectmanagement/application/EmailServiceIntegrationTest.java
@@ -0,0 +1,92 @@
+package org.projectmanagement.application;
+
+
+import com.icegreen.greenmail.configuration.GreenMailConfiguration;
+import com.icegreen.greenmail.junit5.GreenMailExtension;
+import com.icegreen.greenmail.util.GreenMailUtil;
+import com.icegreen.greenmail.util.ServerSetupTest;
+import jakarta.mail.*;
+import jakarta.mail.internet.ContentType;
+import jakarta.mail.internet.MimeMessage;
+import jakarta.mail.internet.MimeMultipart;
+import lombok.extern.slf4j.Slf4j;
+//import org.apache.commons.mail2.jakarta.util.MimeMessageParser;
+import org.apache.commons.mail2.jakarta.util.MimeMessageParser;
+import org.jsoup.Jsoup;
+import org.jsoup.nodes.Document;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.junit.jupiter.api.extension.RegisterExtension;
+import org.projectmanagement.application.dto.mail.InvitationMailBody;
+import org.projectmanagement.domain.services.EmailsService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit.jupiter.SpringExtension;
+import org.springframework.web.util.UriBuilder;
+import org.springframework.web.util.UriComponentsBuilder;
+
+
+import java.io.IOException;
+import java.time.Instant;
+
+import static java.util.concurrent.TimeUnit.SECONDS;
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.awaitility.Awaitility.await;
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+
+@Slf4j
+@SpringBootTest
+@ExtendWith(SpringExtension.class)
+public class EmailServiceIntegrationTest {
+
+    @Autowired
+    private EmailsService emailService;
+
+    @RegisterExtension
+    static GreenMailExtension greenMail = new GreenMailExtension(ServerSetupTest.SMTP_IMAP)
+            .withConfiguration(GreenMailConfiguration
+                    .aConfig()
+                    .withUser("email_integration_test@localhost","tester", "password"))
+            .withPerMethodLifecycle(false);
+
+
+    // Test methods
+    @Test
+    void testSendEmail() throws MessagingException {
+        InvitationMailBody invitationMailBody = new InvitationMailBody("Test company",
+                "Test workspace", "Test user",
+                "Test rolename",false, Instant.now().toEpochMilli());
+        UriBuilder uriBuilder = UriComponentsBuilder.newInstance()
+                .scheme("http")
+                .host("localhost")
+                .port(8080)
+                .path("/invitation");
+        emailService.sendInvitationEmail("email_integration_test@localhost",uriBuilder ,invitationMailBody);
+        greenMail.waitForIncomingEmail(1);
+
+//        try (final Store store = greenMail.getImap().createStore()) {
+//            store.connect("tester", "password");
+//            final Folder inbox = store.getFolder("INBOX");
+//            inbox.open(Folder.READ_ONLY);
+//            assertThat(inbox.getMessageCount()).isEqualTo(1);
+//            MimeMessage message = (MimeMessage) inbox.getMessage(1);
+//            log.info("Email received: {}");
+//            assertThat(message.getSubject()).isEqualTo("Invitation to join project management system");
+//        } catch (MessagingException e) {
+//            log.error("Error while reading email", e);
+//        }
+
+        await().atMost(60*60, SECONDS).untilAsserted(() -> {
+            MimeMessage[] receivedMessages = greenMail.getReceivedMessages();
+            assertEquals(1, receivedMessages.length);
+            MimeMessage receivedMessage = receivedMessages[0];
+            MimeMessageParser parser = new MimeMessageParser(receivedMessage);
+            String getText = Jsoup.parse(parser.parse().getHtmlContent()).text();
+            Document doc = Jsoup.parse(parser.parse().getHtmlContent());
+            log.info("Email received: {}", getText);
+            log.info("Email received: {}", parser.parse().getHtmlContent());
+        });
+    }
+
+}
--- src/test/java/org/projectmanagement/presentation/controllers/CompaniesControllerIntegrationTest.java
+++ src/test/java/org/projectmanagement/presentation/controllers/CompaniesControllerIntegrationTest.java
@@ -1,6 +1,5 @@
 package org.projectmanagement.presentation.controllers;
 
-import com.fasterxml.jackson.core.JsonProcessingException;
 import com.fasterxml.jackson.databind.ObjectMapper;
 import org.junit.jupiter.api.AfterEach;
 import org.junit.jupiter.api.BeforeEach;
@@ -10,7 +9,6 @@ import org.junit.jupiter.api.extension.ExtendWith;
 import org.projectmanagement.application.dto.companies.Company;
 import org.projectmanagement.application.dto.users.UsersLogin;
 import org.projectmanagement.application.exceptions.AppMessage;
-import org.projectmanagement.domain.entities.Companies;
 import org.projectmanagement.domain.services.AuthService;
 import org.projectmanagement.presentation.response.GlobalResponse;
 import org.projectmanagement.test_data_factories.CompaniesDataFactory;
@@ -21,7 +19,6 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.http.MediaType;
-import org.springframework.test.context.ActiveProfiles;
 import org.springframework.test.context.junit.jupiter.SpringExtension;
 import org.springframework.test.web.servlet.MockMvc;
 
