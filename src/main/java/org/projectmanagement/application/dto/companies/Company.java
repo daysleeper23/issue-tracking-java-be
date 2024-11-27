af536d333180d72059d61e1eeadb3f28fb8f9abf
daysleeper23 <charnel1986@gmail.com>
2024-11-28 00:40:05 +0200

    Make Company.userId required

--- src/main/java/org/projectmanagement/application/dto/companies/Company.java
+++ src/main/java/org/projectmanagement/application/dto/companies/Company.java
@@ -7,6 +7,8 @@ import jakarta.validation.constraints.NotBlank;
 public record Company(
         @NotBlank(message = "must not be empty") String name,
         String description,
-        String userId
+
+
+        @NotBlank(message = "must not be empty") String userId
         ) {
 }
